bbbe5c1acf0484f09a0cfb08673978b3198de879
raj deenoo <raj.deenoo@gmail.com>
2023-10-16 22:23:48 +0200

    clean sdl init

--- src/example-tmxparser.cr
+++ src/example-tmxparser.cr
@@ -1,71 +1,44 @@
-require "sdl-crystal-bindings"
-require "sdl-crystal-bindings/sdl-image-bindings"
+require "./sdl-utils"
 
 module Example::Tmxparser
   VERSION = "0.1.0"
 
+  SCREEN_WIDTH  = 640
+  SCREEN_HEIGHT = 480
 
-SCREEN_WIDTH  = 640
-SCREEN_HEIGHT = 480
+  g_window = create_window
+  g_renderer = create_renderer(g_window)
+  init_sdl_image
+  g_texture = load_img_file(g_renderer, "assets/tmw_desert_spacing.png")
 
-if LibSDL.init(LibSDL::INIT_VIDEO) != 0
-  raise "SDL could not initialize! SDL Error: #{String.new(LibSDL.get_error)}"
-end
+  quit = false
 
-if LibSDL.set_hint(LibSDL::HINT_RENDER_SCALE_QUALITY, "1") == 0
-  puts "Warning: Linear texture filtering not enabled!"
-end
-
-g_window = LibSDL.create_window("SDL Tutorial", LibSDL::WINDOWPOS_UNDEFINED, LibSDL::WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, LibSDL::WindowFlags::WINDOW_SHOWN)
-raise "Window could not be created! SDL Error: #{String.new(LibSDL.get_error)}" unless g_window
-
-g_renderer = LibSDL.create_renderer(g_window, -1, LibSDL::RendererFlags::RENDERER_ACCELERATED)
-raise "Renderer could not be created! SDL Error: #{String.new(LibSDL.get_error)}" unless g_renderer
-
-LibSDL.set_render_draw_color(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF)
-
-img_flags = LibSDL::IMGInitFlags::IMG_INIT_PNG
-if (LibSDL.img_init(img_flags) | img_flags.to_i) == 0
-  raise "SDL_image could not initialize! SDL_image Error: #{String.new(LibSDLMacro.img_get_error)}"
-end
-
-loaded_surface = LibSDL.img_load("assets/tmw_desert_spacing.png")
-raise "Unable to load image texture.png! SDL_image Error: #{String.new(LibSDL.get_error)}" unless loaded_surface
-
-g_texture = LibSDL.create_texture_from_surface(g_renderer, loaded_surface)
-raise "Unable to create texture from texture.png! SDL Error: #{String.new(LibSDL.get_error)}" unless g_texture
-
-LibSDL.free_surface(loaded_surface)
-
-quit = false
-
-while (!quit)
-  while LibSDL.poll_event(out e) != 0
-    if e.type == LibSDL::EventType::QUIT.to_i
-      quit = true
+  while (!quit)
+    while LibSDL.poll_event(out e) != 0
+      if e.type == LibSDL::EventType::QUIT.to_i
+        quit = true
+      end
     end
+
+    LibSDL.render_clear(g_renderer)
+
+    tile_width = 8
+    tile_height = 8
+    source_rect = LibSDL::Rect.new(x: tile_width * 19, y: tile_height * 12, w: tile_width, h: tile_height)
+    destination_rect = LibSDL::Rect.new(x: 0, y: 0, w: tile_width * 10, h: tile_height * 10)
+    LibSDL.render_copy(g_renderer, g_texture, pointerof(source_rect), pointerof(destination_rect))
+
+    source_rect = LibSDL::Rect.new(x: tile_width * 20, y: tile_height * 12, w: tile_width, h: tile_height)
+    destination_rect = LibSDL::Rect.new(x: tile_width * 10, y: 0, w: tile_width * 10, h: tile_height * 10)
+    LibSDL.render_copy(g_renderer, g_texture, pointerof(source_rect), pointerof(destination_rect))
+
+    LibSDL.render_present(g_renderer)
   end
 
-  tile_width = 32
-  tile_height = 32
-  source_rect = LibSDL::Rect.new(x: tile_width * 3, y: tile_height * 5, w: tile_width, h: tile_height)
-  destination_rect = LibSDL::Rect.new(x: 0, y: 0, w: tile_width, h: tile_height)
-  # LibSDL.set_render_draw_color(g_renderer, 0x00, 0xFF, 0x00, 0xFF)
-  # LibSDL.render_draw_rect(g_renderer, pointerof(outline_rect))
-  LibSDL.render_clear(g_renderer)
-  LibSDL.render_copy(g_renderer, g_texture, pointerof(source_rect), pointerof(destination_rect))
+  LibSDL.destroy_texture(g_texture)
+  LibSDL.destroy_renderer(g_renderer)
+  LibSDL.destroy_window(g_window)
 
-  LibSDL.render_present(g_renderer)
-
-
-end
-
-LibSDL.destroy_texture(g_texture)
-LibSDL.destroy_renderer(g_renderer)
-LibSDL.destroy_window(g_window)
-
-LibSDL.img_quit
-LibSDL.quit
-
-
+  LibSDL.img_quit
+  LibSDL.quit
 end
---
+++ src/sdl-utils.cr
@@ -0,0 +1,49 @@
+require "sdl-crystal-bindings"
+require "sdl-crystal-bindings/sdl-image-bindings"
+
+module Example::Tmxparser
+  def self.sdl_init
+    if LibSDL.init(LibSDL::INIT_VIDEO) != 0
+      raise "SDL could not initialize! SDL Error: #{String.new(LibSDL.get_error)}"
+    end
+
+    if LibSDL.set_hint(LibSDL::HINT_RENDER_SCALE_QUALITY, "1") == 0
+      puts "Warning: Linear texture filtering not enabled!"
+    end
+  end
+
+  def self.create_window : Pointer(LibSDL::Window)
+    g_window = LibSDL.create_window("SDL Tutorial", LibSDL::WINDOWPOS_UNDEFINED, LibSDL::WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, LibSDL::WindowFlags::WINDOW_SHOWN)
+    raise "Window could not be created! SDL Error: #{String.new(LibSDL.get_error)}" unless g_window
+    
+    g_window
+  end
+
+  def self.create_renderer(window : Pointer(LibSDL::Window)) : Pointer(LibSDL::Renderer)
+    g_renderer = LibSDL.create_renderer(window, -1, LibSDL::RendererFlags::RENDERER_ACCELERATED | LibSDL::RendererFlags::RENDERER_PRESENTVSYNC)
+    raise "Renderer could not be created! SDL Error: #{String.new(LibSDL.get_error)}" unless g_renderer
+
+    LibSDL.set_render_draw_color(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF)
+
+    g_renderer
+  end
+
+  def self.init_sdl_image
+    img_flags = LibSDL::IMGInitFlags::IMG_INIT_PNG
+    if (LibSDL.img_init(img_flags) | img_flags.to_i) == 0
+      raise "SDL_image could not initialize! SDL_image Error: #{String.new(LibSDLMacro.img_get_error)}"
+    end
+  end
+
+  def self.load_img_file(renderer : Pointer(LibSDL::Renderer), file : String) : Pointer(LibSDL::Texture)
+    loaded_surface = LibSDL.img_load(file)
+    raise "Unable to load image #{file}! SDL_image Error: #{String.new(LibSDLMacro.img_get_error)}" unless loaded_surface
+
+    new_texture = LibSDL.create_texture_from_surface(renderer, loaded_surface)
+    raise "Unable to create texture from #{file}! SDL Error: #{String.new(LibSDL.get_error)}" unless new_texture
+
+    LibSDL.free_surface(loaded_surface)
+
+    new_texture
+  end
+end

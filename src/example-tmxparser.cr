f79903452c1e7704b36d638c38d8b2dcfca278b5
raj deenoo <raj.deenoo@gmail.com>
2023-10-18 21:40:45 +0200

    move camera

--- src/example-tmxparser.cr
+++ src/example-tmxparser.cr
@@ -6,9 +6,12 @@ require "./camera"
 module Example::Tmxparser
   VERSION = "0.1.0"
 
-  SCREEN_WIDTH  = 640
-  SCREEN_HEIGHT = 480
-  ZOOM_FACTOR = 2
+# SCREEN_WIDTH  = 640
+# SCREEN_HEIGHT = 480
+  SCREEN_WIDTH  = 1280
+  SCREEN_HEIGHT = 720
+
+  ZOOM_FACTOR = 7
 
   g_window = create_window
   g_renderer = create_renderer(g_window)
@@ -20,7 +23,7 @@ module Example::Tmxparser
 
   sdl_tilemap = SdlTilemap.new(g_renderer, tilemap, "assets")
   sdl_tilemap.load_textures
-  camera = Camera.new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, ZOOM_FACTOR)
+  camera = Camera.new(10, 50, SCREEN_WIDTH, SCREEN_HEIGHT, ZOOM_FACTOR)
 
   quit = false
 
@@ -29,6 +32,23 @@ module Example::Tmxparser
       if e.type == LibSDL::EventType::QUIT.to_i
         quit = true
       end
+    end
+    current_key_states = LibSDL.get_keyboard_state(nil)
+    if current_key_states[LibSDL::Scancode::SCANCODE_ESCAPE.to_i] == 1
+      quit = true
+    end
+    
+    if current_key_states[LibSDL::Scancode::SCANCODE_UP.to_i] == 1
+      camera.y -= 10
+    end
+    if current_key_states[LibSDL::Scancode::SCANCODE_DOWN.to_i] == 1
+      camera.y += 10
+    end
+    if current_key_states[LibSDL::Scancode::SCANCODE_LEFT.to_i] == 1
+      camera.x -= 10
+    end
+    if current_key_states[LibSDL::Scancode::SCANCODE_RIGHT.to_i] == 1
+      camera.x += 10
     end
 
     LibSDL.render_clear(g_renderer)
--- src/sdl-tilemap.cr
+++ src/sdl-tilemap.cr
@@ -46,11 +46,8 @@ module Example::Tmxparser
         puts "layer_data is nil"
         return
       end
-      # puts "layer_data: #{layer_data.inspect}"
       all_data = layer_data.data.split(",").map { |x| begin x.to_i rescue 0 end }
-      # puts "all_data 1: #{all_data.inspect}" if layer.name == "Objects"
       all_texture_source = all_data.map { |x| source_rect_from_tilenumber(x) }
-      # puts "all_data: #{all_texture_source.inspect}" if layer.name == "Objects"
       zoom = camera.value.zoom
       source_tw = (tileset.tilewidth || 1)
       source_th = (tileset.tileheight || 1)
@@ -60,7 +57,12 @@ module Example::Tmxparser
           next if texture_source[0] == -1
           source_rect = LibSDL::Rect.new(x: texture_source[0], y: texture_source[1], w: source_tw, h: source_th)
           dest_x = index_col * source_tw * zoom
-          destination_rect = LibSDL::Rect.new(x: dest_x, y: dest_y, w: source_tw * zoom, h: source_th * zoom)
+          destination_rect = LibSDL::Rect.new(
+            x: dest_x - camera.value.x,
+            y: dest_y - camera.value.y,
+            w: source_tw * zoom,
+            h: source_th * zoom
+          )
           LibSDL.render_copy(@renderer, texture, pointerof(source_rect), pointerof(destination_rect))
         end
       end
@@ -82,24 +84,9 @@ module Example::Tmxparser
     end
 
     def render_map(camera : Pointer(Camera))
-      # puts "render_map"
-      # puts "camera: #{camera.value.width}"
-      # puts @tilemap.layers.last.inspect
       @tilemap.layers.each do |layer|
-        # return unless layer.name == "Objects"
-        # puts "layer: #{layer.name}"
-        # puts "layer data : #{layer.layer_data.inspect}"
         render_layer(layer, camera)
       end
-
-      # tile_width = 8
-      # tile_height = 8
-      # source_rect = LibSDL::Rect.new(x: tile_width * 19, y: tile_height * 12, w: tile_width, h: tile_height)
-      # destination_rect = LibSDL::Rect.new(x: 0, y: 0, w: tile_width * 10, h: tile_height * 10)
-      # # puts "source_rect: #{source_rect.inspect}"
-      # # puts "destination_rect: #{destination_rect.inspect}"
-      # g_texture = @textures["./tilemap.png"]
-      # LibSDL.render_copy(@renderer, g_texture, pointerof(source_rect), pointerof(destination_rect))
     end
 
     def self.load_img_file(renderer : Pointer(LibSDL::Renderer), file : String) : Pointer(LibSDL::Texture)
